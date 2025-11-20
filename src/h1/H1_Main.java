641bf773288ca1e6adefd4917ab1a0f01700b580
unknown <heldheldheldheldheldheldheld@gmail.com>
2025-11-20 16:06:01 +0100

    H1 Arraydarstellung angepasst & H2

--- src/h1/H1_Main.java
+++ src/h1/H1_Main.java
@@ -3,16 +3,17 @@ package h1;
 public class H1_Main {
 
 	public static void main(String[] args) {
-		int[] myArray = {5,6,7,10,11};
+		int[] myArray = {5,6,7,6,10,11,12};
 		int i;
+		int temp;
 		for(i = 0; i < (myArray.length/2); i++) {
-			int temp = myArray[i];
+			temp = myArray[i];
 			myArray[i] = myArray[myArray.length - 1 - i];
 			myArray[myArray.length - 1 - i] = temp;			
 		}
 		System.out.print("myArray: " + myArray[0]);
 		for(i = 1; i < myArray.length; i++) {
-			System.out.print("," + myArray[i]);
+			System.out.print("|" + myArray[i]);
 		}
 	}
 	
---
+++ src/h2/H2_Main.java
@@ -0,0 +1,31 @@
+package h2;
+
+public class H2_Main {
+
+	public static void main(String[] args) {
+		int n = 299;
+		int digits = 5000;
+		int[] a = {1,2,3,4,5,6,7,8,9};
+		a = new int[9];
+		String stringN;
+		stringN = Integer.toString(n);
+		
+		digits = stringN.length();
+		
+		int i;
+		int j;
+		String temp;
+		for(i = a.length - 1, j = 0; i > ((a.length - 1) - digits); i--, j++) {
+			temp = stringN.substring(digits - 1 - j, digits - j);
+			a[i] = Integer.parseInt(temp);
+		}
+		
+		System.out.println("n: " + n);
+		System.out.println("Ziffern: " + digits);
+		System.out.print("a: " + a[0]);
+		for(i = 1; i < a.length; i++) {
+			System.out.print("|" + a[i]);
+		}
+	}
+	
+}
